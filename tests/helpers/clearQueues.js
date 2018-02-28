6535d4a3419425c2648201a022591ed769753427
kirya <cloudkserg11@gmail.com>
2018-02-28 19:30:52 +0300

    https://chronobank.atlassian.net/projects/MD/issues/MD-168 - middleware - add test for check balance after transfer and structure of messages; fix#2

--- tests/helpers/clearQueues.js
+++ tests/helpers/clearQueues.js
@@ -2,5 +2,6 @@ const config = require('../../config');
 
 module.exports = async (amqpInstance) => {
     const channel = await amqpInstance.createChannel();
+    channel.assertQueue(`app_${config.rabbit.serviceName}_test.balance`);
     await channel.purgeQueue(`app_${config.rabbit.serviceName}_test.balance`);
 };
